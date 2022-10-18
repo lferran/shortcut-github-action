fb21ac29555af4a8ff601660c963257f888dbc83
Ferran Llamas <llamas.arroniz@gmail.com>
2022-10-18 07:24:57 +0200

    Fix

--- src/main.py
+++ src/main.py
@@ -8,13 +8,16 @@ def get_github_action_event():
         data = json.load(f)
         return data
 
+def get_shortcut_access_token():
+    return os.environ["SHORTCUT_ACCESS_TOKEN"]
+
 
 def main(args):
     print(args)
     print()
-    print(os.environ)
-    print()
     print(get_github_action_event())
+    print()
+    print(get_shortcut_access_token())
 
 
 if __name__ == "__main__":
